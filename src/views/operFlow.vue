fc45662f0503bff9126a8ecc22febf0595f2c0c2
opteacher <opteacher@hotmail.com>
2025-10-04 03:05:31 +0800

    爬虫操作流设计中

--- src/views/operFlow.vue
+++ src/views/operFlow.vue
@@ -10,13 +10,11 @@ import { useRouter } from 'vue-router'
 import FlowDsgn from '@lib/components/FlowDsgn.vue'
 import { reactive } from 'vue'
 import Node from '@lib/types/node'
-import { v4 as uuid } from 'uuid'
-import { setProp } from '@lib/utils'
 
 const router = useRouter()
 const nodes = reactive<Node[]>([])
 
 function onNodesUpdate(nds: Node[]) {
-  nodes.splice(0, nodes.length, ...nds.map(nd => setProp(nd, 'key', uuid())))
+  nodes.splice(0, nodes.length, ...nds)
 }
 </script>
